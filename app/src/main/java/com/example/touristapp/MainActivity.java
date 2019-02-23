cdc9ef2ec0391987dcbabb91ee47cb13a99f8c46
MrLingo <l.mihaylov@abv.bg>
2019-02-23 15:12:42 +0200

    Google Maps API Markers & Finished welcome screen

--- app/src/main/java/com/example/touristapp/MainActivity.java
+++ app/src/main/java/com/example/touristapp/MainActivity.java
@@ -1,24 +1,63 @@
 package com.example.touristapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+
+    // Create each location by setting it's lat and lon. ( Eventually move to 'Destination Screen' activity later. )
+    public Location LocationFactory(float lat, float lon){
+        Location destination = new Location("");
+        destination.setLatitude(lat);
+        destination.setLongitude(lon);
+
+        return destination;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Button btn = (Button)findViewById(R.id.DestinationsBtn);
+        // Buttons onclick events
+        Button btn = findViewById(R.id.DestinationsBtn);
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, MapsActivity.class));
             }
         });
-    }
-}
+
+        // ( Eventually move to 'Destination Screen' activity later. )
+        Location sofia = LocationFactory(42.691283f,23.318975f );
+        Location plovdiv = LocationFactory(42.144228f,24.736051f );
+        Location nearPlovdiv = LocationFactory(42.144300f, 24.750146f );
+
+        float distanceInMeters = sofia.distanceTo(plovdiv);
+
+        if( distanceInMeters <= 5000 ){
+            Log.i(TAG, "Within distance");
+            // System.out.println("Destination is within radius");
+        }
+        else{
+            //System.out.println("Destination out of radius");
+            Log.i(TAG, "Out of radius");
+        }
+    } // onCreate Method
+}  // Class
--- app/src/main/java/com/example/touristapp/MapsActivity.java
+++ app/src/main/java/com/example/touristapp/MapsActivity.java
@@ -1,7 +1,18 @@
 package com.example.touristapp;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,10 +21,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    LocationManager locationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +36,71 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+
+        /*
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                // Get lat and lng of the device
+                double latitude = location.getLatitude();
+                double longtitude = location.getLongitude();
+                LatLng latLng = new LatLng(latitude, longtitude);
+
+
+
+
+                Geocoder geocoder = new Geocoder(getApplicationContext());
+                try{
+
+                   List<Address> addressList = geocoder.getFromLocation(latitude, longtitude, 1);
+                   String str = addressList.get(0).getLocality() + " , ";
+                   str += addressList.get(0).getCountryName();
+
+                   // Create location marker with title and zoom it.
+                   //mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                   //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7.0f));
+
+                } catch(IOException e){
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        });
+      }
+*/
+
+    } // onCreate method
 
 
     /**
@@ -37,10 +115,84 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        float zoomLevel = 7.0f;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add current GPS location:
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+                    // Get lat and lng of the device
+                    double latitude = location.getLatitude();
+                    double longtitude = location.getLongitude();
+
+                    //LatLng latLng = new LatLng(latitude, longtitude);
+
+                    /*
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try{
+
+                        List<Address> addressList = geocoder.getFromLocation(latitude, longtitude, 1);
+                        String str = addressList.get(0).getLocality() + " , ";
+                        str += addressList.get(0).getCountryName();
+
+                        // Create location marker with title and zoom it.
+                        //mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7.0f));
+
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    }
+                    */
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+        }
+
+        // Add a markers of every destination and move the camera
+
+
+        // Plovdiv destination
+        //LatLng plovdiv = new LatLng(42.144228, 24.736051);
+        //mMap.addMarker(new MarkerOptions().position(plovdiv).title("Destination Plovdiv")).showInfoWindow();
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(plovdiv, zoomLevel));
+
+        // Sofia destination
+        //LatLng sofia = new LatLng(42.691283f, 23.318975f);
+        //mMap.addMarker(new MarkerOptions().position(sofia).title("Destination Sofia")).showInfoWindow();
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(plovdiv, zoomLevel));
+
+        // ...
+
     }
-}
+
+} // Class
+
