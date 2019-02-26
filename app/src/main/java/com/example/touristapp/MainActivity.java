3e938bfdecd9c64def363d53dd637dfdc6055c18
MrLingo <l.mihaylov@abv.bg>
2019-02-26 17:50:04 +0200

    Destinations added on the map

--- app/src/main/java/com/example/touristapp/MainActivity.java
+++ app/src/main/java/com/example/touristapp/MainActivity.java
@@ -6,13 +6,9 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -45,19 +41,87 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // ( Eventually move to 'Destination Screen' activity later. )
-        Location sofia = LocationFactory(42.691283f,23.318975f );
-        Location plovdiv = LocationFactory(42.144228f,24.736051f );
-        Location nearPlovdiv = LocationFactory(42.144300f, 24.750146f );
+        Location carevec = LocationFactory(43.084030f, 25.652586f );
+        Location chydniMostove = LocationFactory(41.819929f, 24.581748f );
+        Location yagodinskaPeshtera = LocationFactory(41.628984f, 24.329589f );
+        Location vruhSnejanka = LocationFactory(41.638506f, 24.675594f );
+        Location belogradchishkiSkali = LocationFactory(43.623361f, 22.677964f );
+        Location peshteraLedenika = LocationFactory(43.204703f, 23.493687f );
+        Location pametnikHristoBotev = LocationFactory(43.798045f, 23.677926f);
+        Location myzeiParahodRadecki = LocationFactory(43.799125f, 23.676921f );
+        Location rezervatKaliakra = LocationFactory(43.361190f, 28.465788f );
+        Location perperikon = LocationFactory(41.718126f, 25.468954f );
+        Location vruhMysala = LocationFactory(42.180021f, 23.585167f);
+        Location vruhShipka = LocationFactory(42.748281f, 25.321387f );
+        Location peshteraSnejanka = LocationFactory(42.004459f, 24.278645f );
+        Location antichenTeatur = LocationFactory(42.147109f, 24.751005f );
+        Location asenovaKrepost = LocationFactory(41.987020f, 24.873552f );
+        Location bachkovskiManastir = LocationFactory(41.942380f, 24.849340f );
+        Location rezervatSreburna = LocationFactory(44.115654f, 27.071807f );
+        Location madarskiKonnik = LocationFactory(43.277708f, 27.118949f );
+        Location sedemteRilskiEzera = LocationFactory(42.203413f, 23.319871f );
+        Location aleksandurNevski = LocationFactory(42.696000f, 23.332879f );
 
-        float distanceInMeters = sofia.distanceTo(plovdiv);
+
+       //Add current GPS location:
+       locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+                    // Get lat and lng of the device
+                    float latitude = (float) location.getLatitude();
+                    float longtitude = (float) location.getLongitude();
+
+                    Location myLocation = LocationFactory(latitude, longtitude );
+
+                } // OnLocationChanged method
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+        }
+        // If GPS connection cannot be established create hardcoded instance
+        else {
+            Location myLocation = LocationFactory(42.131612f, 24.783277f);
+        }
+
+        /*
+        Functionality in "Destination screen activity" for checking wheter location is in the desirable radius:
+
+        float distanceInMeters = myLocation.distanceTo(clickedLocation);
 
         if( distanceInMeters <= 5000 ){
             Log.i(TAG, "Within distance");
-            // System.out.println("Destination is within radius");
         }
         else{
-            //System.out.println("Destination out of radius");
             Log.i(TAG, "Out of radius");
         }
+       */
     } // onCreate Method
 }  // Class
--- app/src/main/java/com/example/touristapp/MapsActivity.java
+++ app/src/main/java/com/example/touristapp/MapsActivity.java
@@ -1,19 +1,8 @@
 package com.example.touristapp;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -32,76 +21,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        /*
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-
-
-        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-
-                // Get lat and lng of the device
-                double latitude = location.getLatitude();
-                double longtitude = location.getLongitude();
-                LatLng latLng = new LatLng(latitude, longtitude);
-
-
-
-
-                Geocoder geocoder = new Geocoder(getApplicationContext());
-                try{
-
-                   List<Address> addressList = geocoder.getFromLocation(latitude, longtitude, 1);
-                   String str = addressList.get(0).getLocality() + " , ";
-                   str += addressList.get(0).getCountryName();
-
-                   // Create location marker with title and zoom it.
-                   //mMap.addMarker(new MarkerOptions().position(latLng).title(str));
-                   //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7.0f));
-
-                } catch(IOException e){
-                    e.printStackTrace();
-                }
-
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        });
-      }
-*/
-
     } // onCreate method
-
 
     /**
      * Manipulates the map once available.
@@ -112,87 +38,95 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         float zoomLevel = 7.0f;
 
-        // Add current GPS location:
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-
-        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-
-                    // Get lat and lng of the device
-                    double latitude = location.getLatitude();
-                    double longtitude = location.getLongitude();
-
-                    //LatLng latLng = new LatLng(latitude, longtitude);
-
-                    /*
-                    Geocoder geocoder = new Geocoder(getApplicationContext());
-                    try{
-
-                        List<Address> addressList = geocoder.getFromLocation(latitude, longtitude, 1);
-                        String str = addressList.get(0).getLocality() + " , ";
-                        str += addressList.get(0).getCountryName();
-
-                        // Create location marker with title and zoom it.
-                        //mMap.addMarker(new MarkerOptions().position(latLng).title(str));
-                        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7.0f));
-
-                    } catch(IOException e){
-                        e.printStackTrace();
-                    }
-                    */
-                }
-
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                }
-
-                @Override
-                public void onProviderEnabled(String provider) {
-
-                }
-
-                @Override
-                public void onProviderDisabled(String provider) {
-
-                }
-            });
-        }
-
         // Add a markers of every destination and move the camera
 
+        // Царевец
+        LatLng carevec = new LatLng(43.084030f, 25.652586f);
+        mMap.addMarker(new MarkerOptions().position(carevec).title("Царевец")).showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(carevec, zoomLevel));
 
-        // Plovdiv destination
-        //LatLng plovdiv = new LatLng(42.144228, 24.736051);
-        //mMap.addMarker(new MarkerOptions().position(plovdiv).title("Destination Plovdiv")).showInfoWindow();
-        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(plovdiv, zoomLevel));
+        // Чудни мостове
+        LatLng chydniMostove = new LatLng(41.819929f, 24.581748f);
+        mMap.addMarker(new MarkerOptions().position(chydniMostove).title("„Чудните Мостове“")).showInfoWindow();
 
-        // Sofia destination
-        //LatLng sofia = new LatLng(42.691283f, 23.318975f);
-        //mMap.addMarker(new MarkerOptions().position(sofia).title("Destination Sofia")).showInfoWindow();
-        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(plovdiv, zoomLevel));
+        // Ягодинска Пещера
+        LatLng yagodinskaPeshtera = new LatLng(41.628984f, 24.329589f);
+        mMap.addMarker(new MarkerOptions().position(yagodinskaPeshtera).title("Ягодинска Пещера")).showInfoWindow();
 
-        // ...
+        // Връх Снежанка
+        LatLng vruhSnejanka = new LatLng(41.638506f, 24.675594f);
+        mMap.addMarker(new MarkerOptions().position(vruhSnejanka).title("Връх Снежанка")).showInfoWindow();
 
-    }
+        // Белоградчишки скали
+        LatLng belogradchiskiSkali = new LatLng(43.623361f, 22.677964f);
+        mMap.addMarker(new MarkerOptions().position(belogradchiskiSkali).title("Белоградчишки Скали")).showInfoWindow();
 
+        // Пещера „Леденика“
+        LatLng peshteraLedenika = new LatLng(43.204703f, 23.493687f);
+        mMap.addMarker(new MarkerOptions().position(peshteraLedenika).title("Пещера „Леденика“")).showInfoWindow();
+
+        // Паметник На Христо Ботев И Неговата Чета
+        LatLng pametneikHristoBotev = new LatLng(43.798045f, 23.677926f);
+        mMap.addMarker(new MarkerOptions().position(pametneikHristoBotev).title("Паметник На Христо Ботев И Неговата Чета")).showInfoWindow();
+
+        // Национален Музей "Параход Радецки"
+        LatLng myzeiParahodRadecki = new LatLng(43.799125f, 23.676921f);
+        mMap.addMarker(new MarkerOptions().position(myzeiParahodRadecki).title("Национален Музей 'Параход Радецки'")).showInfoWindow();
+
+        // Археологически Резерват „Калиакра”
+        LatLng rezervatKaliakra = new LatLng(43.361190f, 28.465788f);
+        mMap.addMarker(new MarkerOptions().position(rezervatKaliakra).title("Археологически Резерват „Калиакра”")).showInfoWindow();
+
+        // Перперикон
+        LatLng perperikon = new LatLng(41.718126f, 25.468954f);
+        mMap.addMarker(new MarkerOptions().position(perperikon).title("Перперикон")).showInfoWindow();
+
+        // Вр. Мусала (2925 М.) - Рила
+        LatLng vruhMysala = new LatLng(42.180021f, 23.585167f);
+        mMap.addMarker(new MarkerOptions().position(vruhMysala).title("Вр. Мусала (2925 М.) - Рила")).showInfoWindow();
+
+        // Връх Шипка – Национален Парк-Музей „Шипка“ - Паметник На Свободата
+        LatLng vruhShipka = new LatLng(42.748281f, 25.321387f);
+        mMap.addMarker(new MarkerOptions().position(vruhShipka).title("Връх Шипка – Национален Парк-Музей „Шипка“ - Паметник На Свободата")).showInfoWindow();
+
+        // Пещера – Пещера „Снежанка“ (Дължина: 145 М)
+        LatLng peshteraSnejanka = new LatLng(42.004459f, 24.278645f);
+        mMap.addMarker(new MarkerOptions().position(peshteraSnejanka).title("Пещера – Пещера „Снежанка“ (Дължина: 145 М)")).showInfoWindow();
+
+        // Античен Театър
+        LatLng antichenTeatur = new LatLng(42.147109f, 24.751005f);
+        mMap.addMarker(new MarkerOptions().position(antichenTeatur).title("Античен Театър")).showInfoWindow();
+
+        // Асенова Крепост
+        LatLng asenovaKrepost = new LatLng(41.987020f, 24.873552f);
+        mMap.addMarker(new MarkerOptions().position(asenovaKrepost).title("Асенова Крепост")).showInfoWindow();
+
+        // Бачковски Манастир
+        LatLng bachkovskiManastir = new LatLng(41.942380f, 24.849340f);
+        mMap.addMarker(new MarkerOptions().position(bachkovskiManastir).title("Бачковски Манастир")).showInfoWindow();
+
+        // Резерват „Сребърна“
+        LatLng rezervatSreburna = new LatLng(44.115654f, 27.071807f);
+        mMap.addMarker(new MarkerOptions().position(rezervatSreburna).title("Резерват „Сребърна“")).showInfoWindow();
+
+        // Мадарски Конник
+        LatLng madarskiKonnik = new LatLng(43.277708f, 27.118949f);
+        mMap.addMarker(new MarkerOptions().position(madarskiKonnik).title("Мадарски Конник")).showInfoWindow();
+
+        // Седемте Рилски езера
+        LatLng sedemteRilskiEzera = new LatLng(42.203413f, 23.319871f);
+        mMap.addMarker(new MarkerOptions().position(sedemteRilskiEzera).title("Седемте Рилски езера")).showInfoWindow();
+
+        //Храм-Паметник „Александър Невски“
+        LatLng aleksandurNevski = new LatLng(42.696000f, 23.332879f);
+        mMap.addMarker(new MarkerOptions().position(aleksandurNevski).title("Храм-Паметник „Александър Невски“")).showInfoWindow();
+
+    } // onMapReady
 } // Class
 
